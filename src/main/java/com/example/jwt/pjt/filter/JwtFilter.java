27fb6eae4d1129fb48907bec0878dec5c1d8bc3c
Dream-Choi <cmb02236@gmail.com>
2025-04-30 15:34:14 +0900

    JWT 검증 실패 시 CORS 헤더 추가 및 오류 메시지 출력 개선

--- src/main/java/com/example/jwt/pjt/filter/JwtFilter.java
+++ src/main/java/com/example/jwt/pjt/filter/JwtFilter.java
@@ -78,9 +78,16 @@ public class JwtFilter implements Filter{
             System.out.println("JWT 검증 성공 컨트롤러로 이동");
             chain.doFilter(request, response);
         }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("토큰의 문제가 생김.");
             System.out.println("JWT 검증 실패");
             res.setStatus(HttpServletResponse.SC_FORBIDDEN);
             res.getWriter().write("Invalid or expired JWT token");
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+            chain.doFilter(request, response);
             return;
         }
     }
